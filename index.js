11f00791dfbbd099e91ce21cec2e68dc2b982fc3
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-09-16 13:44:30 +0300

    almost-done

---
+++ index.js
@@ -0,0 +1,81 @@
+// 1 start
+
+const cel = 17;
+
+console.log(cel)
+
+console.log(cel * 1.8 + 32)
+
+// 1 end
+
+// 2 start
+
+const mounth = 30;
+
+console.log(mounth * 24 * 60)
+
+console.log(mounth * 24)
+
+// 2 end
+
+// 3 start
+const health = 100;
+const energy = 100;
+
+console.log(health - 36)
+console.log(energy - 79)
+// 3 end
+
+// 4 start
+
+const sum = 469;
+
+console.log(sum * 0.9)
+
+// 4 end
+
+// 5 start
+
+// NOT DONE
+
+// 5 end
+
+// 6 start
+
+const floatLine = "56.269";
+
+const floatNumberLine = Number.parseFloat(floatLine)
+
+console.log(floatNumberLine)
+
+// 6 end
+
+// 7 start
+
+const line = "56";
+
+const numberLine = Number.parseInt(line)
+
+console.log(numberLine)
+
+// 7 end
+
+// 8 start
+
+// NOT DONE
+
+// 8 end
+
+// 9 start
+
+const floatToInt = "15.569";
+
+const floatToIntNumber = Number.parseInt(floatToInt)
+
+const result = floatToIntNumber.toString();
+
+console.log(floatToIntNumber)
+
+console.log(result)
+
+// 9 end
